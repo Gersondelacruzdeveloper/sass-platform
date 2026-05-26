c6f27bbcb00a6e8f06362e6ad1998dd2be4c84ca
Gerson <gdelacruz.developer  @gmail.com>
2026-05-25 21:22:28 -0400

    Add frontend to SaaS platform

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import HealthCheckView, DashboardStatsView
-
-
-urlpatterns = [
-    path("health/", HealthCheckView.as_view(), name="health-check"),
-    path("dashboard-stats/", DashboardStatsView.as_view(), name="dashboard-stats"),
-]
--- disco/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Category, Product, StockMovement, Sale, SaleItem, Expense
-
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(StockMovement)
-admin.site.register(Sale)
-admin.site.register(SaleItem)
-admin.site.register(Expense)
