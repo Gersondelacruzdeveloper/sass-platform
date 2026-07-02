6f1643f1030436f1db131d868d6271bfdd4676df
Gerson <gdelacruz.developer  @gmail.com>
2026-07-02 09:56:25 -0400

    Add doamain

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -234,6 +234,19 @@ AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_REGION_NAME = env.str("AWS_S3_REGION_NAME", default="us-east-1")
+# AWS custom domain automation for Ticketing public sites
+AWS_ACM_REGION = env.str("AWS_ACM_REGION", default="us-east-1")
+
+AWS_CLOUDFRONT_DISTRIBUTION_ID = env.str(
+    "AWS_CLOUDFRONT_DISTRIBUTION_ID",
+    default="",
+)
+
+AWS_CLOUDFRONT_DOMAIN = env.str(
+    "AWS_CLOUDFRONT_DOMAIN",
+    default="",
+)
+
 
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
