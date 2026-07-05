4d30c1e8f06349995030ce3112be1b7cd6684d80
Gerson <gdelacruz.developer  @gmail.com>
2026-07-04 23:20:48 -0400

    Add slugyfy to import

--- backend/ticketing/views.py
+++ backend/ticketing/views.py
@@ -21,6 +21,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.utils.dateparse import parse_date
+from django.utils.text import slugify
 
 logger = logging.getLogger(__name__)
 
